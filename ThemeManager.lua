dd06796d2af84117fffca78e7c8c5c37caf7b107
tho0s <xavierkylex@gmail.com>
2026-02-01 16:06:39 +0500

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -49,7 +49,7 @@ do
     ThemeManager.BuiltInThemes = {
         ["Default"] = {
             1,
-            { FontColor = "ffffff", MainColor = "191919", AccentColor = "7d55ff", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
+            { FontColor = "ffffff", MainColor = "191919", AccentColor = "8f8f8f", BackgroundColor = "0f0f0f", OutlineColor = "282828" },
         },
         ["BBot"] = {
             2,
